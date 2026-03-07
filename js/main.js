d82b8cde57c970311b46adf26714862760adfea5
Adeliina <adeliina.lampela@gmail.com>
2026-03-07 14:46:25 +0100

    delete comment drafts

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ if (menuButton) {
   // check the "open" state
   menuButton.addEventListener("click", () => {
     if (navLinks.classList.contains("open")) {
-      navLinks.classList.remove("open"); // Close the menu
+      navLinks.classList.remove("open"); // close the menu
     } else {
       navLinks.classList.add("open"); // Open the menu
     }
